a5c1e5e5869303fc8d17a8dedef4984361e5ecd0
arleyxD <castearley@gmail.com>
2023-07-26 20:14:35 -0500

    Se Arregla modelo de TiendaProductos se agrega relacion

--- src/models/Tienda_ProductoFrutas.js
+++ src/models/Tienda_ProductoFrutas.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Tienda_ProductosFrutasSchema = Schema({
-    Id_Tienda: { type: String, required: true },
-    Id_Frutas: { type: String, required: true}
+    Id_Tienda: { type: Schema.Types.ObjectId, ref:'Tiendas', required :true },
+    Id_Frutas: { type: Schema.Types.ObjectId, ref:'Frutas',required: true}
 });
 
 module.exports = mongoose.model('Tienda_ProductosFrutas', Tienda_ProductosFrutasSchema);
