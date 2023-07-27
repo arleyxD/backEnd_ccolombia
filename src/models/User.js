e1c2b05a1cce57ab266c6d03af2a729b2426491b
arleyxD <castearley@gmail.com>
2023-07-26 21:06:33 -0500

    se modifica el modelo de user

--- src/models/User.js
+++ src/models/User.js
@@ -20,7 +20,6 @@ const UserSchema = new Schema(
             required: true,
             default: 'registered'
         },
-        pedidos: [{ type: Schema.Types.ObjectId, ref: 'Pedidos' }],
     },{
         timestamps: true
     }
