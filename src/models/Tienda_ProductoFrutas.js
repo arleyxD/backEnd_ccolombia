44671b379b12733e3202a24ae771bfa38c597aa5
arleyxD <castearley@gmail.com>
2023-07-26 20:35:20 -0500

    Arreglo Rama

--- src/models/Tienda_ProductoFrutas.js
+++ src/models/Tienda_ProductoFrutas.js
@@ -4,5 +4,4 @@ const Tienda_ProductosFrutasSchema = Schema({
     Id_Tienda: { type: Schema.Types.ObjectId, ref:'Tiendas', required :true },
     Id_Frutas: { type: Schema.Types.ObjectId, ref:'Frutas',required: true}
 });
-
 module.exports = mongoose.model('Tienda_ProductosFrutas', Tienda_ProductosFrutasSchema);
