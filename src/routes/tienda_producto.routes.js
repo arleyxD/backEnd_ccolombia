1796d6fa9d70d750d047970d412e6835af6de49b
CORTAVO2015 <gustavoancortes@gmail.com>
2023-07-26 20:48:05 -0500

    Se arreglo routes de tiendaproductos

--- src/routes/tienda_producto.routes.js
+++ src/routes/tienda_producto.routes.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const Tienda_Productos = require('../controllers/Tienda_Productos.controller');
 
-router.get('/', Tienda_Productos.getFrutas);
-router.get('/:id', Tienda_Productos.getFruta);
-router.post('/', Tienda_Productos.createFruta);
-router.put('/:id', Tienda_Productos.updateFruta);
-router.delete('/:id', Tienda_Productos.deleteFruta);
+router.get('/', Tienda_Productos.getTiendas);
+router.get('/:id', Tienda_Productos.getTienda);
+router.post('/', Tienda_Productos.createTienda);
+router.put('/:id', Tienda_Productos.updateTienda);
+router.delete('/:id', Tienda_Productos.deleteTienda);
 
 module.exports = router;
