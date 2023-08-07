321bbbac5cf7997c8c860876267d40dadfb72ee0
arleyxD <castearley@gmail.com>
2023-08-07 14:20:27 -0500

    Se arregla inventario campos en el modelo

--- src/models/invemtario.js
+++ src/models/invemtario.js
@@ -4,9 +4,13 @@ const { Schema } = mongoose;
 const inventarioSchema = new Schema({
     id_tienda: { type: Schema.Types.ObjectId, ref: 'Tiendas', required: true },
     id_fruta: { type: Schema.Types.ObjectId, ref: 'Frutas', required: true },
-    cantidad_disponible: { type: Number, required: true },
-    precio: { type: Number, required: true },
-    unidad: { type: String, required: true }
+    tipo:{type:String},
+    inventarioCanastilla:{ type: Number},
+    inventarioBulto:{ type: Number},
+    valorMedioKilo: { type: Number, required: true },
+    valorKilo:{ type: Number, required: true },
+    valorC:{ type: Number},
+    valorB:{ type: Number}
   });
 
   module.exports = mongoose.model('Inventario', inventarioSchema);
