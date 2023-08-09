283d037eb953b1d0ece33762e9a0e24edfda60c8
arleyxD <castearley@gmail.com>
2023-08-09 09:38:57 -0500

    [+] se arrglo routes tienda token

--- src/routes/tienda.routes.js
+++ src/routes/tienda.routes.js
@@ -7,7 +7,7 @@ const { validateToken } = require('../middlewares/validate-jwt.middleware');
 router.get('/', tiendaController.getTiendas);
 router.get('/:id', tiendaController.getTienda);
 router.get('/user/:id', tiendaController.getTiendaUser);
-router.post('/', validateToken, tiendaController.createTienda);
+router.post('/', tiendaController.createTienda);
 router.put('/:id', validateToken, tiendaController.updateTienda);
 router.delete('/:id', validateToken, tiendaController.deleteTienda);
 
